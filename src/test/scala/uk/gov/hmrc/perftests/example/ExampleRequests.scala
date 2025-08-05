7e87d1f445c77344b244593e50a1081690ae32c8
wayne ellis <9093361+funkmaster20th@users.noreply.github.com>
2025-08-05 19:03:30 +0100

    PLATUI-3801 updated to performance test runner 2.6.0

--- src/test/scala/uk/gov/hmrc/perftests/example/ExampleRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/ExampleRequests.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2023 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.perftests.example
@@ -24,13 +35,13 @@ object ExampleRequests extends ServicesConfiguration {
   val postVatReturnPeriod: HttpRequestBuilder =
     http("Post VAT return Period")
       .post(s"$baseUrl$route/vat-return-period": String)
-      .formParam("vatReturnPeriod", s"$${vatReturnPeriod}")
-      .formParam("csrfToken", s"$${csrfToken}")
+      .formParam("vatReturnPeriod", s"#{vatReturnPeriod}")
+      .formParam("csrfToken", s"#{csrfToken}")
       .check(status.is(303))
       .check(header("Location").is("/check-your-vat-flat-rate/turnover").saveAs("turnOverPage"))
 
   val getTurnoverPage: HttpRequestBuilder =
     http("Get Turnover Page")
-      .get(s"$baseUrl$${turnOverPage}": String)
+      .get(s"$baseUrl#{turnOverPage}": String)
       .check(status.is(200))
 }
